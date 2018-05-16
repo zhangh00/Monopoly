2ae53386d8fa0ec59f6f791e17e5ec534b688d82
zhangh00 <39269045+zhangh00@users.noreply.github.com>
2018-05-16 09:34:02 +0200

    Menu erledigt & Start beim Spielfeld

---
+++ Monopoly/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Monopoly
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Single(object sender, RoutedEventArgs e)
+        {
+            Spiel subWindow = new Spiel(1);
+            subWindow.Show();
+            this.Close();
+        }
+        private void Multi(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            int zahl = Convert.ToInt16(button.Content);
+            Spiel subWindow = new Spiel(zahl);
+            subWindow.Show();
+            this.Close();
+        }
+    }
+}
---
+++ Monopoly/Spieler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly
+{
+    class Spieler
+    {
+        private int coin;
+        private int spielerid;
+        private int spielerstatus;
+        public Spieler(int zahl)
+        {
+            //
+        }
+    }
+}
